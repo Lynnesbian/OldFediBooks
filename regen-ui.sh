7015dede109923b4c6176deb9e9841e65e79309b
Lynne <thecoolone3@gmail.com>
2019-02-09 09:58:53 +1000

    don't regenerate the uic file if there haven't been any changes

--- regen-ui.sh
+++ regen-ui.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-for f in gui/*.ui; do echo $f; pyside2-uic -d $f -o FediBooks/uic/ui_${f:4:-2}py; done
+for f in gui/*.ui; do
+out=FediBooks/uic/ui_${f:4:-2}py
+if [ ! -f "$out" ] || [ "$f" -nt "$out" ]; then
+	echo $f ($out)
+	pyside2-uic -i 0 -d $f -o $out
+fi
+done
