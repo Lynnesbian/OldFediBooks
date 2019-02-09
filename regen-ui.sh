cc84bcb88e5369869fab527c3aad8fa718a81857
Lynne <thecoolone3@gmail.com>
2019-02-09 10:44:16 +1000

    fixed syntax error

--- regen-ui.sh
+++ regen-ui.sh
@@ -2,7 +2,7 @@
 for f in gui/*.ui; do
 out=FediBooks/uic/ui_${f:4:-2}py
 if [ ! -f "$out" ] || [ "$f" -nt "$out" ]; then
-	echo $f ($out)
+	echo $f \($out\)
 	pyside2-uic -i 0 -d $f -o $out
 fi
 done
